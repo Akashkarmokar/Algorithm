c6d6b62119e91b92ac1a2812b240e98a2935f862
Akash Karmokar <akashcsemu@gmail.com>
2018-07-18 12:34:38 +0600

    Add files via upload

---
+++ kmp algorithm.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<cstring>
+//#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+
+int lps[50];
+
+int build_lps(char *pat,int sizee){   //It's the failure table to analyze the pattern
+    int i =0,j=1;
+    while(j<sizee){
+        if(pat[i]==pat[j]){
+            lps[j]=i+1;
+            j++;
+            i++;
+        }
+        else{
+            if(i!=0){
+                i--;
+            }
+            else{
+                lps[j]=0;
+                j++;
+            }
+        }
+    }
+}
+/*
+Here we pass string  and pattern .
+*/
+
+int kmp (char *str,char *ptr){    // It's  this algorithm's principal  method
+
+    int p_size = strlen(ptr);
+    int s_size = strlen(str);
+
+    build_lps(ptr,p_size);
+
+    int str_index=0,ptr_index=0;
+
+
+    while (str_index<s_size ){
+        if(str[str_index]==ptr[ptr_index]){
+            while(str[str_index]==ptr[ptr_index] && str_index < s_size){
+                str_index++;
+                ptr_index++;
+
+            }
+            if(ptr[ptr_index]=='\0'){
+                cout << "Index found at " << str_index-ptr_index << " position "  << endl;
+                ptr_index=0;
+            }
+            else{
+                int lps_value = lps[ptr_index-1];
+
+                if(lps_value == 0){
+                //str_index++;
+                ptr_index = 0;
+                }
+                else{
+                ptr_index = lps_value;
+                }
+            }
+
+        }
+        else{
+            str_index++;
+
+        }
+    }
+}
+int main()
+{
+    lps[0]=0;
+
+    char* str="baaabab";
+    char* ptr = "ba";
+
+    kmp(str,ptr);
+
+    return 0;
+}
