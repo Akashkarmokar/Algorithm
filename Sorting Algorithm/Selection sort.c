ea7c7a4aad779fb8b564d6b694e95c5817f00fcb
Akash Karmokar <akashcsemu@gmail.com>
2018-08-25 03:58:08 +0600

    Add files via upload

--- Sorting Algorithm/Selection sort.c
+++ Sorting Algorithm/Selection sort.c
@@ -1,24 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int selection[]={5,2,6,3,4};
-    int n=5,i,j,temp,min_index;
-    for(i=0;i<n-1;i++){
-        min_index = i;
-        for(j=i+1;j<n;j++){
-            if(selection[j]<selection[min_index]){
-                min_index = j;
+    int i=1,j,temp,item_no;
+    int array[5]={5,4,2,3,1};
+    //temp=array[0];
+    for(i=1;i<=5-1;i++){
+        for(j=i;j>=0;j--){
+            if(array[j]<array[j-1]){
+                temp=array[j];
+                array[j]=array[j-1];
+                array[j-1]=temp;
             }
         }
-        if(i!=min_index){
-            temp = selection[i];
-            selection[i]=selection[min_index];
-            selection[min_index]=temp;
-        }
     }
-    for(i =0;i<n;i++){
-        printf("%d\n",selection[i]);
+    for(int i=0;i<5;i++){
+        printf("%d",array[i]);
     }
-
-    return 0;
 }
