9b5386d3ef67b3906570b981d276dbd2065ea6b3
Akash Karmokar <akashcsemu@gmail.com>
2020-02-28 01:47:37 +0600

    Create josephus_algorithm_with_loop_and_recursion.cpp

---
+++ Modular Arithmatic/josephus_algorithm_with_loop_and_recursion.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// using recursion
+int josephus_recursion(int people,int count_number)
+{
+	if(people==1)
+		return 1;
+	else
+		return (josephus_recursion(people-1,count_number)+(count_number-1))%people+1;
+}
+
+
+int main()
+{
+	int people,count_number;
+	cin>>people>>count_number;
+	cout<<"Output with recursion function"<<endl;
+	cout<<josephus_recursion(people,count_number);
+	cout<<"-------"<<endl;
+
+	cout<<"OUtput with loop"<<endl;
+
+	int initial_soward = 1;
+	for(int j=2;j<=people;j++)
+	{
+		initial_soward = (initial_soward+(count_number-1))%j+1;
+	}
+	cout<<initial_soward<<endl;
+	return 0;
+}
+
+
+
+
+// for more details : https://www.geeksforgeeks.org/josephus-problem-set-1-a-on-solution/
